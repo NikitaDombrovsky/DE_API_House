70c247d8fd8ff4ddd17fc2b7916d4e42384cf917
NikitaDombrovky <nikita.nikita55@mail.ru>
2022-05-16 23:16:51 +0600

    FinalMemes

--- app/src/main/java/com/example/de_api/MainActivity.java
+++ app/src/main/java/com/example/de_api/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // String str = "";
+        //MessageDigest dig = MessageDigest.getInstance("SHA-256");
+        //byte[] st1 = dig.digest(str.getBytes();
     }
 }
--- app/src/main/java/com/example/de_api/RoomActivity.java
+++ app/src/main/java/com/example/de_api/RoomActivity.java
@@ -10,9 +10,13 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.common.hash.Hashing;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+
+import java.nio.charset.StandardCharsets;
 
 public class RoomActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
@@ -21,6 +25,11 @@ public class RoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+
+        // TODO HASH
+        final String hashed = Hashing.sha256()
+                .hashString("your input", StandardCharsets.UTF_8)
+                .toString();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);//fragmentContainerView
